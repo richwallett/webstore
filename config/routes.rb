a97bf3f5004459cc6f29ba38219bf5504e895f67
LPL <lukelolla@gmail.com>
2013-04-08 21:15:09 -0700

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,14 @@ Webstore::Application.routes.draw do
   root :to => "sessions#new"
   resources :sessions, only: [:create, :new] do
     collection do
-      delete "destroy"
+      delete "destroy" #REV: hm... Multiple sessions?
     end
   end
   resources :users
   resources :products
   resources :carts, except: [:index, :show, :destroy] do
     collection do
-      get "show"
+      get "show" #REV: guess I need to read the controller to understand this
       delete "destroy"
     end
   end
